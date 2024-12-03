30c9323ac97fc186ddc99abfe364594c364ce4d1
Thanh829 <93962044+ThanhNIT@users.noreply.github.com>
2024-12-03 21:38:28 +0700

    Update solution.test.js

--- src/test/js/g0001_0100/s0006_zigzag_conversion/solution.test.js
+++ src/test/js/g0001_0100/s0006_zigzag_conversion/solution.test.js
@@ -1,6 +1,6 @@
 // tslint:disable:no-magic-numbers
 import { expect, test } from 'vitest'
-import { convert } from '../../../../main/js/g0001_0100/s0006_zigzag_conversion/Solution'
+import { convert } from '../../../../main/js/g0001_0100/s0006_zigzag_conversion/solution'
 
 test('convert', () => {
     expect(convert('PAYPALISHIRING', 3)).toEqual('PAHNAPLSIIGYIR')
